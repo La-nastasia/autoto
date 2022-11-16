0f1e83ff888f1b04f87a5c1b20caa6a2db7a3acd
La-nastasia <117431519+La-nastasia@users.noreply.github.com>
2022-11-16 14:04:09 +0200

    Message

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.blog.models import BlogCategory, Artcile
+from apps.blog.models import BlogCategory, Article, Tag
 admin.site.register(BlogCategory)
-admin.site.register(Artcile)
+admin.site.register(Article)
+admin.site.register(Tag)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -9,11 +9,12 @@ class BlogCategory(models.Model):
         verbose_name='Категория блога'
         verbose_name_plural='Категории блога'
 
-class Artcile(models.Model):
+class Article(models.Model):
     category=models.ForeignKey(to=BlogCategory,verbose_name='Категории', on_delete=models.CASCADE)
     title=models.CharField(verbose_name='Заголовок',max_length=255)
     text_preview=models.TextField(verbose_name='Текст-превью',null=True,blank=True)
     text=models.TextField(verbose_name='Текст')
+    tags = models.ManyToManyField('Tag',blank=True,verbose_name='Tеги')
     publish_date=models.DateTimeField(verbose_name='Дата публикации')
     updated_at=models.DateTimeField(verbose_name='Дата изменения',auto_now=True)
     created_at=models.DateTimeField(verbose_name='Дата создания',auto_now_add=True)
@@ -24,3 +25,13 @@ class Artcile(models.Model):
     class Meta:
         verbose_name='Статья'
         verbose_name_plural='Статьи'
+class Tag(models.Model):
+    name=models.CharField(verbose_name='Хештег',max_length=255)
+    articles = models.ManyToManyField(Article, blank=True, verbose_name='Статьи с такими тегами')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Tег'
+        verbose_name_plural='Tеги'
