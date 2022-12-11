57395bc4273472edb8fdc9751f5b2f84fa8d1fa3
La-nastasia <117431519+La-nastasia@users.noreply.github.com>
2022-12-11 12:17:32 +0200

    blog part 2 tags

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello,this is a main page :)')
+    return render(request, 'index.html')
