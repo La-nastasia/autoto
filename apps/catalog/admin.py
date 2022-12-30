22c0c64644499d18d12b159af544ce62d439c4da
La-nastasia <117431519+La-nastasia@users.noreply.github.com>
2022-12-30 18:01:56 +0200

    products relations and catalog views

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -4,6 +4,10 @@ from apps.catalog.models import Category,Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':['name']}
+class ProductCategoryInline(admin.TabularInline):
+    model= Product.categories.through
+    extra=1
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
+    inlines=[ProductCategoryInline]
