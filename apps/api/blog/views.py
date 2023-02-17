96f6b27d7594a6530882359c5feb0071c76a73c4
La-nastasia <117431519+La-nastasia@users.noreply.github.com>
2023-02-17 18:10:40 +0200

    drf part 4

--- apps/api/blog/views.py
+++ apps/api/blog/views.py
@@ -1,39 +1,45 @@
-from rest_framework import generics, permissions
-from apps.blog.models import Article
-from apps.api.blog.serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions, status
+from apps.blog.models import Article, BlogCategory, Tag
+from apps.api.blog.serializers import BlogCategorySerializer, ArticleWriteSerializer, ArticleReadSerializer
 
-class ArticleListView(generics.ListAPIView):
-    serializer_class = ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleReadSerializer
+    queryset = Article.objects.all()
     def get_queryset(self):
-        queryset = Article.objects.all()
+        queryset= Article.objects.all()
         category = self.request.query_params.get('category')
         if category:
             queryset = queryset.filter(category=category)
         user = self.request.query_params.get('user')
         if user:
             queryset = queryset.filter(user=user)
+        title = self.request.query_params('title')
+        if title:
+            queryset = queryset.filter(title__icontains=title)
         return queryset
 
-class ArticleDetailView(generics.RetrieveAPIView):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-
-
-class ArticleCreateView(generics.CreateAPIView):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-    permission_classes = [permissions.IsAdminUser]
+    def get_serializer_class(self):
+        if self.action in ['create', 'update']:
+            return ArticleWriteSerializer
+        return self.serializer_class
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(user= self.request.user)
 
-
-class ArticleUpdateView(generics.UpdateAPIView):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-    permission_classes = [permissions.IsAdminUser]
-
-
-class ArticleDeleteView(generics.DestroyAPIView):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-    permission_classes = [permissions.IsAdminUser]
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            return [permission() for permission in[permissions.IsAdminUser]]
+        return [permission() for permission in[permissions.AllowAny]]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tag = []
+        for tag_name in serializer.validated_data.get('tag'):
+            tag_1 = Tag.objects.filter(name=tag_name).first()
+            if not tag_1:
+                tag_1 =Tag.objects.create(name=tag_name)
+            tag.append(tag_1)
+        article = serializer.save(user=self.request.user, tag=tag)
+        read_serializer = self.serializer_class(article, context = {'request': request})
+        return Response(read_serializer.data, status=status.HTTP_201_CREATED)
